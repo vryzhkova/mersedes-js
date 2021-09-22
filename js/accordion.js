01fcd8ee368dd7340e9beac1cbea752b67e89dcd
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2021-09-22 17:18:35 +0300

    add accordion

---
+++ js/accordion.js
@@ -0,0 +1,16 @@
+const lists = document.querySelectorAll('.feature-sub');
+const btns = document.querySelectorAll('.feature__link');
+
+btns.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        btns.forEach((btnItem, idx) => {
+            if (btnItem === btn) {
+                btnItem.classList.toggle('feature__link_active')
+                lists[idx].classList.toggle('hidden')
+            } else {
+                btnItem.classList.remove('feature__link_active')
+                lists[idx].classList.add('hidden')
+            }
+        })
+    })
+})
